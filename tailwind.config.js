83dfc45239466c003be3e5b9d6848159dd4b726f
kyoheiehime089-cpu <kyohei.ehime089@gmail.com>
2026-05-11 08:28:10 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,9 @@
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          DEFAULT: '#FFD600',
-          dark: '#C7A000'
-        }
-      }
-    }
+    extend: {},
   },
-  plugins: []
-};
+  plugins: [],
+}
