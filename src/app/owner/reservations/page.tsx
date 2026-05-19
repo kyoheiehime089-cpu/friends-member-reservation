ec4e314784d51f900155ad71382d068eadafad65
kyoheiehime089-cpu <kyohei.ehime089@gmail.com>
2026-05-19 12:33:55 +0900

    Use final owner calendar directly

--- src/app/owner/reservations/page.tsx
+++ src/app/owner/reservations/page.tsx
@@ -1,4 +1,4 @@
-import { OwnerCalendarReliable as OwnerReservationsCalendar } from '@/components/OwnerCalendarReliable';
+import { OwnerCalendarFinal as OwnerReservationsCalendar } from '@/components/OwnerCalendarFinal';
 
 export default function Page() {
   return <OwnerReservationsCalendar />;
